30ae70bd90a43bbf37fd64f08d0dd088dbdf47d4
He Chen <chenhe95@hotmail.com>
2017-01-21 18:58:45 -0500

    2-image search + skeletonization

--- examples/imgexp.py
+++ examples/imgexp.py
@@ -6,8 +6,8 @@ except:
     HOUGH_GRADIENT = cv2.HOUGH_GRADIENT
 import sys
 import numpy as np
-from skimage.morphology import binary_closing
-from skimage.morphology import disk
+from skimage.morphology import skeletonize
+from skimage import img_as_ubyte
 
 if __name__ == "__main__":
     file_name = sys.argv[1]
@@ -19,20 +19,17 @@ if __name__ == "__main__":
         width, height = img.shape
     img = cv2.blur(img, (5, 5))
     cv2.imshow('blur', img)
-    ret3, thresh_img_1 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    _, thresh_img_1 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     thresh_img_2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 13, -5)
     thresh_img_3 = cv2.morphologyEx(thresh_img_2, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
     thresh_img_3_blur = cv2.blur(thresh_img_3, (8, 8))
-    thresh_img_4 = cv2.erode(thresh_img_3_blur, np.ones((2, 2), np.uint8), iterations=6)
-    # thresh_img_4 = cv2.morphologyEx(thresh_img_3_blur, cv2.MORPH_CLOSE, np.ones((8, 8), np.uint8))
-    ret3, thresh_img_4 = cv2.threshold(thresh_img_4, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    # se1 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-    # se2 = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
-    # mask = cv2.morphologyEx(thresh_img_2, cv2.MORPH_CLOSE, se1)
-    # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, se2)
-    # thresh_img_4 = mask
-cv2.imshow('opening', thresh_img_3)
-cv2.imshow('img3blur', thresh_img_3_blur)
-cv2.imshow('final', thresh_img_4)
+    thresh_img_4 = cv2.erode(thresh_img_3_blur, np.ones((2, 2), np.uint8), iterations=3)
+    _, thresh_img_5 = cv2.threshold(thresh_img_4, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    thresh_img_6 = img_as_ubyte(skeletonize(thresh_img_5 != 255))
+    _, thresh_img_7 = cv2.threshold(thresh_img_6, 0, 255, cv2.THRESH_BINARY_INV) 
+
+
+cv2.imshow('circle detect image', thresh_img_5) # use this image to detect circles/nodes
+cv2.imshow('graph search image', thresh_img_7) # use this image to detect connectivity
 cv2.waitKey(0)
 cv2.destroyAllWindows()
