0283551e27410ae6c3391b88e4dc9294c7b3c921
He Chen <chenhe95@hotmail.com>
2017-01-21 23:55:17 -0500

    load_uji()

--- p2l/experimental/textRecognition/training-scripts/load_dataset.py
+++ p2l/experimental/textRecognition/training-scripts/load_dataset.py
@@ -8,33 +8,31 @@ try: # Compatibility with different OpenCV versions.
 except:
     HOUGH_GRADIENT = cv2.HOUGH_GRADIENT
 
-path = os.path.join("..", "data", "uji")
-train_files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+def load_uji():
+	path = os.path.join("..", "data", "uji")
+	train_files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
-X, y = [], []
+	X, y = [], []
 
-width, height = 1300, 2100
-num_pair = re.compile(r"\s*\d+\s+\d+\s*")
+	width, height = 1300, 2100
+	num_pair = re.compile(r"\s*\d+\s+\d+\s*")
 
-for train_file in train_files:
-	with open(os.path.join(path, train_file), "r") as dataset:
-		build = []
-		for line in dataset:
-			if ".SEGMENT CHARACTER" in line:
-				y.append(line[line.find("\"") + 1:line.find("\"", line.find("\"") + 1)])
-			elif ".PEN_UP" in line and build:
-				img = np.zeros((height, width), np.uint8)
-				for t in xrange(len(build) - 1):
-					x1 = build[t]
-					x2 = build[t + 1]
-					cv2.line(img, x1, x2, 255)
-				X.append(cv2.resize(img, (650, 1050)))
-				build = []
-			elif num_pair.match(line):
-				coordinates = [int(x) for x in line.split()]
-				build.append(tuple(coordinates))
-# print y[0]
-# cv2.imshow("img", X[0])
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+	for train_file in train_files:
+		with open(os.path.join(path, train_file), "r") as dataset:
+			build = []
+			for line in dataset:
+				if ".SEGMENT CHARACTER" in line:
+					y.append(line[line.find("\"") + 1:line.find("\"", line.find("\"") + 1)])
+				elif ".PEN_UP" in line and build:
+					img = np.zeros((height, width), np.uint8)
+					for t in xrange(len(build) - 1):
+						x1 = build[t]
+						x2 = build[t + 1]
+						cv2.line(img, x1, x2, 255)
+					X.append(cv2.resize(img, (650, 1050)))
+					build = []
+				elif num_pair.match(line):
+					coordinates = [int(x) for x in line.split()]
+					build.append(tuple(coordinates))
+	return X, y
 
