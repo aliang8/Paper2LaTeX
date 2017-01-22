3c09db96a7b65e99691f3999f8f0e5b5d6372b26
He Chen <chenhe95@hotmail.com>
2017-01-21 23:54:30 -0500

    load_dataset fix

--- p2l/experimental/textRecognition/training-scripts/load_dataset.py
+++ p2l/experimental/textRecognition/training-scripts/load_dataset.py
@@ -1,11 +1,19 @@
 import os
+import numpy as np
 import re
+import cv2
+try: # Compatibility with different OpenCV versions.
+    import cv2.cv as cv
+    HOUGH_GRADIENT = cv.CV_HOUGH_GRADIENT
+except:
+    HOUGH_GRADIENT = cv2.HOUGH_GRADIENT
 
 path = os.path.join("..", "data", "uji")
 train_files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
 X, y = [], []
 
+width, height = 1300, 2100
 num_pair = re.compile(r"\s*\d+\s+\d+\s*")
 
 for train_file in train_files:
@@ -14,9 +22,19 @@ for train_file in train_files:
 		for line in dataset:
 			if ".SEGMENT CHARACTER" in line:
 				y.append(line[line.find("\"") + 1:line.find("\"", line.find("\"") + 1)])
-			elif ".PEN_DOWN" in line and build:
-				X.append(build)
+			elif ".PEN_UP" in line and build:
+				img = np.zeros((height, width), np.uint8)
+				for t in xrange(len(build) - 1):
+					x1 = build[t]
+					x2 = build[t + 1]
+					cv2.line(img, x1, x2, 255)
+				X.append(cv2.resize(img, (650, 1050)))
+				build = []
 			elif num_pair.match(line):
-				build.append(tuple(line.split()))
+				coordinates = [int(x) for x in line.split()]
+				build.append(tuple(coordinates))
+# print y[0]
+# cv2.imshow("img", X[0])
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
-print y
